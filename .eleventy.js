fc074afd4da96043bbc30cdb389834d37df41844
matatabiii <27771489+matatabiii@users.noreply.github.com>
2021-07-04 11:25:32 +0900

    sassの導入

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,25 @@
 module.exports = function(eleventyConfig) {
-  // Config API を使って新たなフィルター（次回以降解説予定）を追加する
-  eleventyConfig.addFilter( "myFilter", function() {});
-  eleventyConfig.addPassthroughCopy("assets");
+  // Config API を使って新たなフィルター追加する
+  // eleventyConfig.addFilter('myFilter', function() {});
+
+  // assetsの対象ファイルをoutputディレクトリにコピー
+  eleventyConfig.addPassthroughCopy('./src/assets/images');
+
+  // watch対象の追加
+  eleventyConfig.addWatchTarget('./src/assets/sass/');
+  eleventyConfig.addWatchTarget('./src/assets/js/');
+
+  // BrowserSyncのoptionを上書き
+  eleventyConfig.setBrowserSyncConfig({
+    notify: false
+  });
 
   // オブジェクトで設定してた値は`return`に指定する（オプション）
   return {
     dir: {
-      input: "src",
-      output: "dist"
+      input: 'src',
+      output: 'dist'
     },
-    // templateFormats: ["md", "njk"] // 対象のファイルを限定する
+    // templateFormats: ['md', 'njk'] // 対象のファイルを限定する
   };
 };
